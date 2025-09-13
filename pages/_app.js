4332bdccb5a0efdcc0252031ef820b8154891157
Pakistaniand <pakistanigamestore@gmail.com>
2025-09-13 05:12:41 -0700

    Create _app.js

---
+++ pages/_app.js
@@ -0,0 +1,10 @@
+import '../styles.css'
+import { CartProvider } from '../contexts/CartContext'
+
+export default function MyApp({ Component, pageProps }) {
+  return (
+    <CartProvider>
+      <Component {...pageProps} />
+    </CartProvider>
+  )
+}
