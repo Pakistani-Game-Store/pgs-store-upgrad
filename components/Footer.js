982afbe893b502ebfdb83a2d6af9265f9d9d5262
Pakistaniand <pakistanigamestore@gmail.com>
2025-09-13 05:16:39 -0700

    Create Footer.js

---
+++ components/Footer.js
@@ -0,0 +1,13 @@
+export default function Footer() {
+  return (
+    <footer className="footer">
+      <div className="inner">
+        <div>
+          <div style={{fontWeight:800}}>Pakistan Game Store</div>
+          <div className="small">Play • Wear • Own</div>
+        </div>
+        <div className="small muted">📧 pakistanigamestore@gmail.com • Payments: Easypaisa | JazzCash | Sadapay | FirstPay | Opay | ABHI</div>
+      </div>
+    </footer>
+  );
+}
